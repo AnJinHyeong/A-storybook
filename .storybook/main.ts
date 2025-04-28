dccd29f256083162b8780f9f9f1a45c7592618cb
안진형 <wlsgud357@naver.com>
2025-04-29 00:05:26 +0900

    rollback

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,29 +2,23 @@ import path from 'path';
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-  stories: [
+  "stories": [
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  addons: [
+  "addons": [
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions"
   ],
-  framework: {
-    name: "@storybook/react-webpack5",
-    options: {}
+  "framework": {
+    "name": "@storybook/react-webpack5",
+    "options": {}
   },
-  staticDirs: [
+  "staticDirs": [
     path.resolve(__dirname, '../public')
-  ],
-  webpackFinal: async (config) => {
-    if (config.output) {
-      config.output.publicPath = '/storybook/';
-    }
-    return config;
-  }
+  ]
 };
 
 export default config;
