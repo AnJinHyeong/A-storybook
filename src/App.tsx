0017e43a61387ce38b3d45150125906e2d29f13e
안진형 <wlsgud357@naver.com>
2025-05-27 14:40:59 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          모두가 기다리는 시간
         </p>
         <a
           className="App-link"
